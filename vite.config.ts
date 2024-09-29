3e953208307d1c0391f1c88d7baf3ccc175f2d21
AchrafLarbi <larbiachraf03@gmail.com>
2024-09-29 12:56:10 +0100

    add: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: 'https://achraflarbi.github.io/Portfolio/',
+  base: '/Portfolio/',
   plugins: [
     react(),
   ],
