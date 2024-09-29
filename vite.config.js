687f864e5228232a280ba1c710ca6cdbd30be0be
AchrafLarbi <larbiachraf03@gmail.com>
2024-09-29 14:08:59 +0100

    add: deploy workflow

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: "https://AchrafLarbi.github.io/Portfolio/",
+  plugins: [react()],
+  assetsInclude: ["**/*.JPG", "**/*.jpg", "**/*.png", "**/*.svg", "**/*.PNG"], // Add this line
+  server: {
+    open: true, // automatically open the app in the browser
+    port: 5174,
+  },
+  resolve: {
+    alias: {
+      screens: path.resolve(__dirname, "./src/screens"),
+    },
+  },
+  build: {
+    outDir: "dist",
+  },
+});
